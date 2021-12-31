0706055c82a33ea6d835ed81eeeb05646789fe76
Tadeo Bustamante <tadeohb@gmail.com>
2021-12-31 13:35:04 -0700

    Removed hardcoded MongoDB URI
    Removed hardcoded MongoDB URI and configured to retrieve it from an environmental variable called 'MONGODB_URI' instead.

--- config/dev.py
+++ config/dev.py
@@ -1,8 +1,9 @@
 from config import *
+from os import getenv
 
 # Set the App Enviroment to Development
 APP_ENV = APP_ENV_DEVELOPMENT
 
 # Database configuration
-QUEST_DB_URI = "mongodb+srv://m001-student:BPMiqYDSvq5clMJM@quest.fi3hg.mongodb.net/quest"
+QUEST_DB_URI = getenv("MONGODB_URI")
 QUEST_DB_NAME = "test_quest"
